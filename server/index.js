7585f75bf35d6d95b02672c98518425fecb015af
Valerie PerezPimentel <vperezpimentel@xpanxion.com>
2023-06-07 11:06:35 -0400

    ex 1

--- server/index.js
+++ server/index.js
@@ -1,8 +1,9 @@
 const cors = require('cors')
 const express = require('express')
+const pool = require('./PromisePool').promisePool
 const { body, check, param, validationResult } = require('express-validator')
 
-const PORT = 80
+const PORT = 8080
 const app = express()
 const corsOptions = { origin: ['http://localhost:3000'], optionsSuccessStatus: 200 }
 
@@ -12,6 +13,18 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 
 // Your endpoints here..
+app.get('/message', cors(corsOptions), async(req, res) => {
+    res.send({message: 'Hello world!!'})
+})
+
+//ex#2
+
+// app.get('/cars/:id', cors(corsOptions), async(req, res) =>{
+    //const result = await pool.query('SELECT * FROM cars)
+    // const body = result[0]
+    // res.send(body)
+
+// }
 
 
 app.listen(PORT, () => {
